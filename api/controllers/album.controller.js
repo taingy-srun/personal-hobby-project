f7496896c524d1f3f0961fdd394921e493482973
taingy-srun <taingy.srun20@gmail.com>
2023-05-17 22:18:50 -0500

    update controller to use named callback with promises

--- api/controllers/album.controller.js
+++ api/controllers/album.controller.js
@@ -1,17 +1,48 @@
+const { response } = require("express");
 const mongoose = require("mongoose");
 const Album = mongoose.model(process.env.ALBUM_MODEL);
 
-const _sendErrorResponse = function(res, err) {
-    res.status(parseInt(process.env.HTTP_ERROR));
-    res.json({message: err.message});
+const _createResponse = function(status, message) {
+    const response = {
+        status: status,
+        message: message
+    }
+    return response;
 }
 
-const _sendResponse = function(res, res_code, message) {
-    res.status(parseInt(res_code));
-    res.json(message);
+const _setResponse = function(response, status, message) {
+    response.status = status;
+    response.message = message;
+}
+
+const _sendResponse = function(res, response) {
+    res.status(parseInt(response.status));
+    res.json(response.message);
+}
+
+const _checkAlbumExist = function(album) {
+    return new Promise((resolve, reject) => {
+        if (!album) {
+            reject({message: process.env.HTTP_ALBUM_NOT_FOUND_MESSAGE});
+        } else {
+            resolve(album);
+        }
+    });
+}
+
+const _checkSongExist = function(song) {
+    return new Promise((resolve, reject) => {
+        if (!song) {
+            reject({message: process.env.HTTP_SONG_NOT_FOUND_MESSAGE});
+        } else {
+            resolve(song);
+        }
+    });
 }
 
 const getAll = function(req, res) {
+    const response = _createResponse();
+
     let offset = process.env.DEFAULT_OFFSET;
     let count = process.env.DEFAULT_COUNT;
     if (req.query && req.query.offset) {
@@ -21,156 +52,138 @@ const getAll = function(req, res) {
         count = parseInt(req.query.count);
     }
     if (count > parseInt(process.env.MAX_COUNT)) {
-        _sendResponse(res, process.env.HTTP_BAD_REQUEST, { message: process.env.HTTP_BAD_REQUEST_MESSAGE_EXCEED_COUNT });
+        _setResponse(response, process.env.HTTP_BAD_REQUEST, { message: process.env.HTTP_BAD_REQUEST_MESSAGE_EXCEED_COUNT });
+        _sendResponse(res, response);
         return;
     }
 
-    Album.find().skip(offset).limit(count).sort({releaseDate: 1}).exec().then(function(albums) {
-        _sendResponse(res, process.env.HTTP_OK, albums);
-    }).catch(function(err) {
-        _sendErrorResponse(res, err);
-    });
+    Album.find().skip(offset).limit(count).sort({releaseDate: 1}).exec()
+        .then((albums) => _setResponse(response, process.env.HTTP_OK, albums))
+        .catch((error) => _setResponse(response, process.env.HTTP_ERROR, {message: error.message}))
+        .finally(() => _sendResponse(res, response));
 };
 
 const getOne = function(req, res) {
+    const response = _createResponse();
     const id = req.params.id;
-    Album.findById(id).exec().then(function(album) {
-        if (!album) {
-            _sendResponse(res, process.env.HTTP_NOT_FOUND, {message: process.env.HTTP_ALBUM_NOT_FOUND_MESSAGE});
-        } else {
-            _sendResponse(res, process.env.HTTP_OK, album);
-        }
-    }).catch(function(err) {
-        _sendErrorResponse(res, err);
-    });
+    Album.findById(id).exec()
+        .then((album) => _checkAlbumExist(album))
+        .then((album) => _setResponse(response, process.env.HTTP_OK, album))
+        .catch((error) => _setResponse(response, process.env.HTTP_NOT_FOUND, error))
+        .finally(() => _sendResponse(res, response));
 }
 
 const deleteOne = function(req, res) {
+    const response = _createResponse();
     const id = req.params.id;
-    Album.findByIdAndDelete(id).exec().then(function(album) {
-        if (!album) {
-            _sendResponse(res, process.env.HTTP_NOT_FOUND, {message: process.env.HTTP_ALBUM_NOT_FOUND_MESSAGE});
-        } else {
-            _sendResponse(res, process.env.HTTP_OK, {message : process.env.ALBUM_DELETED_MESSAGE});
-        }
-    }).catch(function(err) {
-        _sendErrorResponse(res, err);
-    });
+    Album.findByIdAndDelete(id).exec()
+        .then((album) => _checkAlbumExist(album))
+        .then((album) => _setResponse(response, process.env.HTTP_OK, {message : process.env.ALBUM_DELETED_MESSAGE}))
+        .catch((error) => _setResponse(response, process.env.HTTP_NOT_FOUND, error))
+        .finally(() => _sendResponse(res, response));
 }
 
 const addOne = function(req, res) {
+    const response = _createResponse();
     const newAlbum = {
         title: req.body.title,
         releaseDate: req.body.releaseDate,
         numberOfSongs: req.body.numberOfSongs,
         songs: req.body.songs
     }
-    Album.create(newAlbum).then(function(album) {
-        _sendResponse(res, process.env.HTTP_OK, {message : process.env.ALBUM_ADDED_MESSAGE});
-    }).catch(function(err) {
-        _sendErrorResponse(res, err);
-    });
+    Album.create(newAlbum)
+        .then((album) => _setResponse(response,process.env.HTTP_OK, {message: process.env.ALBUM_ADDED_MESSAGE}))
+        .catch((error) => _setResponse(response, process.env.HTTP_ERROR, error.message))
+        .finally(() => _sendResponse(res, response));
 }
 
 const _updateOne = function(req, res, updateOneCallback) {
+    const response = _createResponse();
     const id = req.params.id;
-    Album.findById(id).exec().then(function(album) {
-        if (!album) {
-            _sendResponse(res, process.env.HTTP_NOT_FOUND, {message: process.env.HTTP_ALBUM_NOT_FOUND_MESSAGE});
-        } else {
-            updateOneCallback(req, res, album);
-        }
-    }).catch(function(err) {
-        _sendErrorResponse(res, err);
-    });
+    Album.findById(id).exec()
+        .then((album) => _checkAlbumExist(album))
+        .then((album) => updateOneCallback(req, res, album, response))
+        .catch((error) => _setResponse(response, process.env.HTTP_NOT_FOUND, error))
+        .finally(() => _sendResponse(res, response));
 }
 
-const _fullUpdateOne = function(req, res, album) {
+const _fullUpdateOne = function(req, res, album, response) {
     album.title = req.body.title;
     album.releaseDate = req.body.releaseDate;
     album.songs = req.body.songs
 
-    album.save().then(function(saved){
-        _sendResponse(res, process.env.HTTP_OK, {message: process.env.ALBUM_UPDATED_MESSAGE});
-    }).catch(function(err) {
-        _sendErrorResponse(res, err);
-    });
+    album.save()
+        .then((album) => _setResponse(response, process.env.HTTP_OK, {message: process.env.ALBUM_UPDATED_MESSAGE}))
+        .catch((error) => _setResponse(response, process.env.HTTP_ERROR, error))
+        .finally(() => _sendResponse(res, response));
 }
 
 const fullUpdateOne = function(req, res) {
     _updateOne(req, res, _fullUpdateOne);
 }
 
-const _partialUpdateOne = function(req, res, album) {
+const _partialUpdateOne = function(req, res, album, response) {
     if (req.body.title) { album.title = req.body.title; }
     if (req.body.releaseDate) { album.releaseDate = req.body.releaseDate; }
     if (req.body.songs) { album.songs = req.body.songs }
 
-    album.save().then(function(saved){
-        _sendResponse(res, process.env.HTTP_OK, {message: process.env.ALBUM_UPDATED_MESSAGE});
-    }).catch(function(err) {
-        _sendErrorResponse(res, err);
-    });
+    album.save()
+        .then((album) => _setResponse(response, process.env.HTTP_OK, {message: process.env.ALBUM_UPDATED_MESSAGE}))
+        .catch((error) => _setResponse(response, process.env.HTTP_ERROR, error))
+        .finally(() => _sendResponse(res, response));
 }
 
 const partialUpdateOne = function(req, res) {
     _updateOne(req, res, _partialUpdateOne);
 }
 
+
 const getAlbumSongs = function(req, res) {
+    const response = _createResponse();
     const id = req.params.id;
-    Album.findById(id).select(process.env.SONG_COLLECTION).exec().then(function(songs) {
-        if (!songs) {
-            _sendResponse(res, process.env.HTTP_NOT_FOUND, {message: process.env.HTTP_ALBUM_NOT_FOUND_MESSAGE});
-        } else {
-            _sendResponse(res, process.env.HTTP_OK, songs);
-        }
-    }).catch(function(err) {
-        _sendErrorResponse(res, err);
-    });
+    Album.findById(id).select(process.env.SONG_COLLECTION).exec()
+        .then((album) => _checkAlbumExist(album))
+        .then((songs) => _setResponse(response, process.env.HTTP_OK, songs))
+        .catch((error) => _setResponse(response, process.env.HTTP_NOT_FOUND, error))
+        .finally(() => _sendResponse(res, response));
 }
 
 const getOneSong = function(req, res) {
+    const response = _createResponse();
     const id = req.params.id;
-    Album.findById(id).exec().then(function(album) {
-        if (album && album.songs.id(req.params.songId)) {
-            _sendResponse(res, process.env.HTTP_OK, album.songs.id(req.params.songId));
-        } else {
-            _sendResponse(res, process.env.HTTP_NOT_FOUND, {message: process.env.HTTP_SONG_NOT_FOUND_MESSAGE});
-        }
-    }).catch(function(err) {
-        _sendErrorResponse(res, err);
-    });
+    Album.findById(id).exec()
+        .then((album) => _checkAlbumExist(album))
+        .then((album) => _checkSongExist(album.songs.id(req.params.songId)))
+        .then((song) => _setResponse(response, process.env.HTTP_OK, song))
+        .catch((error) => _setResponse(response, process.env.HTTP_NOT_FOUND, error))
+        .finally(() => _sendResponse(res, response));
 }
 
 const _updateOneSong = function(req, res, updateOneSongCallback) {
+    const response = _createResponse();
     const id = req.params.id;
-    Album.findById(id).select(process.env.SONG_COLLECTION).exec().then(function(album) {   
-        if (!album || !album.songs.id(req.params.songId)) {
-            _sendResponse(res, process.env.HTTP_NOT_FOUND, {message: process.env.HTTP_SONG_NOT_FOUND_MESSAGE});
-        } else {
-            updateOneSongCallback(req, res, album);
-        }
-    }).catch(function(err) {
-        _sendErrorResponse(res, err);
-    });
+    Album.findById(id).select(process.env.SONG_COLLECTION).exec()
+        .then((album) => _checkAlbumExist(album))
+        .then((album) => _checkSongExist(album.songs.id(req.params.songId)))
+        .then((song) => updateOneSongCallback(req, res, song, response))
+        .catch((error) => _setResponse(response, process.env.HTTP_NOT_FOUND, error))
+        .finally(() => _sendResponse(res, response));
 }
 
 const fullUpdateOneSong = function(req, res) {
     _updateOneSong(req, res, _fullUpdateOneSong);
 }
 
-const _fullUpdateOneSong = function(req, res, album) {
+const _fullUpdateOneSong = function(req, res, album, response) {
     const song = {
         title: req.body.title,
         duration: req.body.duration
     }
     album.songs.id(req.params.songId).set(song);
-    album.save().then(function(album) {
-        _sendResponse(res, process.env.HTTP_OK, {message: process.env.SONG_UPDATED_MESSAGE});
-    }).catch(function(err){
-        _sendErrorResponse(res, err);
-    });
+    album.save()
+        .then((album) => _setResponse(response, process.env.HTTP_OK, {message: process.env.SONG_UPDATED_MESSAGE}))
+        .catch((error) => _setResponse(response, process.env.HTTP_ERROR, error))
+        .finally(() => _sendResponse(res, response));
 }
 
 const partialUpdateOneSong = function(req, res) {
@@ -183,54 +196,47 @@ const _partialUpdateOneSong = function(req, res, album) {
     if (req.body.duration) { song.duration = req.body.duration }
 
     album.songs.id(req.params.songId).set(song);
-    album.save().then(function(album) {
-        _sendResponse(res, process.env.HTTP_OK, {message: process.env.SONG_UPDATED_MESSAGE});
-    }).catch(function(err) {
-        _sendErrorResponse(res, err);
-    });
+    album.save()
+        .then((album) => _setResponse(response, process.env.HTTP_OK, {message: process.env.SONG_UPDATED_MESSAGE}))
+        .catch((error) => _setResponse(response, process.env.HTTP_ERROR, error))
+        .finally(() => _sendResponse(res, response));
+}
+
+const _deleteSongFromAlbum = function(album, song) {
+    album.songs.remove(song);
+    return album.save();
 }
 
 const deleteOneSong = function(req, res) {
+    const response = _createResponse();
     const id = req.params.id;
     const songId = req.params.songId;
-    Album.findById(id).exec().then(function(album) {
-        if (album && album.songs.id(songId)) {
-            const song = album.songs.id(songId);
-            album.songs.remove(song);
-            album.save().then(function(updatedAlbum) {
-                _sendResponse(res, process.env.HTTP_OK, {message: process.env.SONG_DELETED_MESSAGE});
-            }).catch(function(err) {
-                _sendErrorResponse(res, err);
-            });
-        } else {
-            _sendResponse(res, process.env.HTTP_NOT_FOUND, {message: process.env.HTTP_SONG_NOT_FOUND_MESSAGE});
-        }
-    }).catch(function(err) {
-        _sendErrorResponse(res, err);
-    });
+    Album.findById(id).exec()
+        .then((album) => _checkAlbumExist(album))
+        .then((album) => _deleteSongFromAlbum(album, album.songs.id(songId)))
+        .then(() => _setResponse(response, process.env.HTTP_OK, {message: process.env.SONG_DELETED_MESSAGE}))
+        .catch((error) => _setResponse(response, process.env.HTTP_NOT_FOUND, error))
+        .finally(() => _sendResponse(res, response));
 }
 
-const addOneSong = function(req, res) {
-    const id = req.params.id;
+const _addSongToAlbum = function(album, req) {
     const song = {
         title: req.body.title,
         duration: req.body.duration
     }
+    album.songs.push(song);
+    return album.save();
+}
 
-    Album.findById(id).exec().then(function(album) {   
-        if (!album) {
-            _sendResponse(res, process.env.HTTP_NOT_FOUND, {message: process.env.HTTP_ALBUM_NOT_FOUND_MESSAGE});
-        } else {
-            album.songs.push(song);
-            album.save().then(function(album) {
-                _sendResponse(res, process.env.HTTP_OK, {message: process.env.SONG_ADDED_MESSAGE});
-            }).catch(function(err) {
-                _sendErrorResponse(res, err);
-            });
-        }
-    }).catch(function(err) {
-        _sendErrorResponse(res, err);
-    });
+const addOneSong = function(req, res) {
+    const response = _createResponse();
+    const id = req.params.id;
+    Album.findById(id).exec()
+        .then((album) => _checkAlbumExist(album))
+        .then((album) => _addSongToAlbum(album, req))
+        .then(() => _setResponse(response, process.env.HTTP_OK, {message: process.env.SONG_ADDED_MESSAGE}))
+        .catch((error) => _setResponse(response, process.env.HTTP_ERROR, error))
+        .finally(() => _sendResponse(res, response));
 }
 
 module.exports = {
