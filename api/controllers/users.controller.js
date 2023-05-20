17c3a24fa8d2e4f26a75ec5f6cfc6ff1eb672c92
taingy-srun <taingy.srun20@gmail.com>
2023-05-20 00:30:33 -0500

    update api

--- api/controllers/users.controller.js
+++ api/controllers/users.controller.js
@@ -64,7 +64,11 @@ const _isPasswordMatch = function(isMatch, user) {
 
 const _generateToken = function(user) {
     const sign = util.promisify(jwt.sign);
-    return sign({name: user.name}, process.env.TOKEN_SECRET, {expiresIn: parseInt(process.env.TOKEN_EXPIRE_IN)})   
+    return new Promise((resolve, reject) => {
+        sign({name: user.name}, process.env.TOKEN_SECRET, {expiresIn: parseInt(process.env.TOKEN_EXPIRE_IN)})
+            .then((token) => resolve({token: token, user: user}))
+            .catch((error) => reject(error));
+    });
 }
 
 const addOne = function(req, res) {
@@ -85,7 +89,7 @@ const getOne = function(req, res) {
         .then((user) =>  _comparePassword(req.body.password, user))
         .then(({isMatch, user}) => _isPasswordMatch(isMatch, user))
         .then((user) => _generateToken(user))
-        .then((token) => _setResponse(response, process.env.HTTP_OK, {token: token}))
+        .then(({token, user}) => _setResponse(response, process.env.HTTP_OK, {name: user.name, token: token}))
         .catch(() => _setResponse(response, process.env.HTTP_UNAUTHORIZED, {message: process.env.HTTP_UNAUTHORIZED_MESSAGE}))
         .finally(() => _sendResponse(res, response));
 }
